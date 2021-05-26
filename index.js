5e32de924941e1b127046ea551d04870a8ab3c58
Sumei Kom <sumeikom@gmail.com>
2021-05-25 20:30:04 -0400

    commit tree bf1ade35cf28580570a21ffeea14262990076512

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(num1, num2) {
+    return 2000 / 100
+    
+  }
+
+  const square = function(x) {
+      return x * x
+  }
+ 
+  const add = function(a, b){
+      return a + b
+  }
